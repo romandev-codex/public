93fd23247e435c3e717c53695ee84aab3970590e
romandev-codex <romandev.codex@gmail.com>
2026-04-02 13:57:02 +0300

    Update start_vast.sh

--- gpu/start_vast.sh
+++ gpu/start_vast.sh
@@ -2,3 +2,8 @@
 
 set -e -o pipefail
 
+pip install --upgrade pip
+pip install uv
+pip install --no-cache-dir aiohttp vastai-sdk --disable-pip-version-check
+pip install --no-cache-dir fastapi uvicorn --disable-pip-version-check
+pip install packaging
