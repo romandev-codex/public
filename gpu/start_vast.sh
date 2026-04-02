f5e178a4de33e3184d1413d9be8ee50ffaceea3f
romandev-codex <romandev.codex@gmail.com>
2026-04-02 14:00:41 +0300

    Update start_vast.sh

--- gpu/start_vast.sh
+++ gpu/start_vast.sh
@@ -7,3 +7,13 @@ pip install uv
 pip install --no-cache-dir aiohttp vastai-sdk --disable-pip-version-check
 pip install --no-cache-dir fastapi uvicorn --disable-pip-version-check
 pip install packaging
+
+mkdir -p /workspace
+
+wget https://raw.githubusercontent.com/romandev-codex/public/refs/heads/main/gpu/service.py -O /workspace/service.py
+nohup python /workspace/service.py > output.log 2>&1 &
+
+wget https://raw.githubusercontent.com/romandev-codex/public/refs/heads/main/gpu/worker.py -O /workspace/worker.py
+nohup python /workspace/worker.py > output.log 2>&1 &
+
+echo "END"
