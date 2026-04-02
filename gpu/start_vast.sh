26a7764aa777be7f1e5c614d1dbe9a57b75516ad
romandev-codex <romandev.codex@gmail.com>
2026-04-02 19:23:10 +0300

    Update start_vast.sh

--- gpu/start_vast.sh
+++ gpu/start_vast.sh
@@ -4,7 +4,7 @@ set -e -o pipefail
 # https://github.com/vast-ai/pyworker/blob/main/workers/comfyui-json/worker.py
 # wget -O - "https://raw.githubusercontent.com/romandev-codex/public/refs/heads/main/gpu/start_vast.sh" | bash
 
-echo "V1.1.2"
+echo "V1.1.3"
 
 # Install deps
 pip install --no-cache-dir aiohttp vastai-sdk fastapi uvicorn packaging --disable-pip-version-check
@@ -14,7 +14,11 @@ wget https://raw.githubusercontent.com/romandev-codex/public/refs/heads/main/gpu
 nohup uvicorn service:app --host 0.0.0.0 --port 3010 --reload > uvicorn.log 2>&1 &
 
 # Run worker
-wget https://raw.githubusercontent.com/romandev-codex/public/refs/heads/main/gpu/worker.py -O worker.py
-exec python3 worker.py
+# wget https://raw.githubusercontent.com/romandev-codex/public/refs/heads/main/gpu/worker.py -O worker.py
+# exec python3 worker.py
+
+# Run worker
+wget https://raw.githubusercontent.com/romandev-codex/public/refs/heads/main/gpu/mock-worker.py -O mock-worker.py
+exec python3 mock-worker.py
 
 echo "END"
