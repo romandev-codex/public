7ecd4a93d825fbfce723b7d35157be3815ca5df8
romandev-codex <romandev.codex@gmail.com>
2026-04-02 14:19:35 +0300

    Update start_vast.sh

--- gpu/start_vast.sh
+++ gpu/start_vast.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
-
-# wget -O - "https://raw.githubusercontent.com/romandev-codex/public/refs/heads/main/gpu/start_vast.sh" | bash
-
 set -e -o pipefail
 
-echo "V1"
+echo "V1.1.2"
 
+# Fix Jupyter-related issues
+mkdir -p /workspace
+cd /workspace
+
+unset JUPYTER_CONFIG_DIR
+unset JUPYTER_PATH
+unset JUPYTER_RUNTIME_DIR
+
+export HOME=/workspace
+export JUPYTER_RUNTIME_DIR=/workspace
+export JUPYTER_SERVER_ROOT=/workspace
+
+# Install deps
 pip install --upgrade pip
 pip install uv
-pip install --no-cache-dir aiohttp vastai-sdk --disable-pip-version-check
-pip install --no-cache-dir fastapi uvicorn --disable-pip-version-check
-pip install packaging
+pip install --no-cache-dir aiohttp vastai-sdk fastapi uvicorn packaging --disable-pip-version-check
 
-mkdir -p /workspace
+# Start service
+wget https://raw.githubusercontent.com/romandev-codex/public/refs/heads/main/gpu/service.py -O service.py
+nohup python service.py > service.log 2>&1 &
 
-wget https://raw.githubusercontent.com/romandev-codex/public/refs/heads/main/gpu/service.py -O /workspace/service.py
-nohup python /workspace/service.py > service.log 2>&1 &
-
-wget https://raw.githubusercontent.com/romandev-codex/public/refs/heads/main/gpu/worker.py -O /workspace/worker.py
-python /workspace/worker.py
+# Run worker
+wget https://raw.githubusercontent.com/romandev-codex/public/refs/heads/main/gpu/worker.py -O worker.py
+python worker.py
 
 echo "END"
